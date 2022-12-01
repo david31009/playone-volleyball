a3ba24f4123186ebef3bb9cd8a95d07972240017
david31009 <david31009@gmail.com>
2022-12-01 20:24:39 +0800

    fix: add rate limit test

--- test/test_ratelimiter.js
+++ test/test_ratelimiter.js
@@ -1,8 +1,9 @@
 const axios = require('axios');
+require('dotenv').config({ path: '../.env' });
 
 for (let i = 0; i < 501; i++) {
   axios
-    .get('http://localhost:3000/api/1.0/group')
+    .get(`${process.env.IP}api/1.0/group`)
     .then((result) => {
       console.log(result.status);
     })
